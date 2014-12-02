6f51d8df891e828b35cf97a94b144af89fc71564
vudu <vudu@mmcolo.attlocal.net>
2014-12-02 17:41:41 -0600

    using fusion helper

--- lib/custom_helper.rb
+++ lib/custom_helper.rb
@@ -1,11 +1,13 @@
 module CustomHelper
-
+	def phone
+		"210-529-7170"
+	end
 	def titleize title
     ActiveSupport::Inflector.titleize(title)
   end
 	
 	def fusion
-		"Fusion Salon"
+		'<span class="fusion" title="Call us at ' + phone + '" data-toggle="tooltip">Fusion Salon</span>'
 	end
 
 	def prices which
